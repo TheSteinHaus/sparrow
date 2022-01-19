8bf5299887c2fd193980c7cfd547826bc8363cd2
TheSteinHaus <semen.skopin@gmail.com>
2022-01-19 16:59:35 +1000

    registration is done

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -23,8 +23,7 @@ class UserController {
             const {email, password} = req.body;
             const userData = await userService.login(email, password);
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
-            // return res.json(userData);
-            return 
+            return res.json(userData);
         } catch (e) {
             next(e);
         }
